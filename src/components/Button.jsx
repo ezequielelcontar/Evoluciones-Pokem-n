c055a328f19f5075cbf4e3ce9928b85e4be2c269
ezebastian <sebastian.ezequiel.elcontar.1991@gmail.com>
2023-12-08 02:12:13 -0300

    secondcommit

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import '../sass/Button.scss'
+
+const Button = (props) => {
+  return (
+    <div className='button__box'>
+        <button className="button">{props.text}</button>
+    </div>
+    
+  )
+}
+
+export {Button}
